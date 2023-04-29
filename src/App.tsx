a40139cffd4e28fe188e6b801c0486a0bdff343c
John Doty <johnrdoty92@gmail.com>
2023-04-29 21:23:29 +0900

    set default block position to middle of screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,61 @@
 import { Container } from './components/Layout/Container';
 import { DevControls } from './components/DevControls';
-import { Block } from 'components/Block';
 import { useBlockContext } from 'contexts/BlockContext';
 import classes from './css/App.module.css';
+import { useEffect, useRef } from 'react';
+import { animated, useSprings } from '@react-spring/web';
 
 const BLOCK_COUNT = 3;
 
 function App() {
-    const { state } = useBlockContext();
-    return (
-        <Container>
-            <DevControls />
-            <div className={classes.blockContainer} style={{ ...state.parent }}>
-                {Array(BLOCK_COUNT)
-                    .fill(<></>)
-                    .map((_, i) => (
-                        <Block key={i} />
-                    ))}
-            </div>
-        </Container>
-    );
+  const dataBlockContainerRef = useRef<HTMLDivElement>(null);
+  const { state } = useBlockContext();
+  const [springs, api] = useSprings(BLOCK_COUNT, () => {
+    return {
+      x: window.innerWidth / 2,
+      y: window.innerHeight / 2,
+    };
+  });
+
+  useEffect(() => {
+    if (!dataBlockContainerRef.current) return;
+    dataBlockContainerRef.current.childNodes.forEach((child, i) => {
+      if (!(child instanceof HTMLElement)) return;
+      api.start((index) => {
+        if (index !== i) return false;
+        return {
+          x: child.offsetLeft,
+          y: child.offsetTop,
+        };
+      });
+    });
+  }, [state]);
+
+  return (
+    <Container>
+      <DevControls />
+      {springs.map((props, i) => {
+        return (
+          <animated.div
+            className={classes.visibleBlock}
+            key={`visibleBlock-${i}`}
+            style={{ ...props }}
+          />
+        );
+      })}
+      <div
+        ref={dataBlockContainerRef}
+        className={classes.blockContainer}
+        style={{ ...state.parent }}
+      >
+        {Array(BLOCK_COUNT)
+          .fill(<></>)
+          .map((_, i) => (
+            <div key={`dataBlock-${i}`} className={classes.dataBlock} />
+          ))}
+      </div>
+    </Container>
+  );
 }
 
 export default App;
--- src/components/DevControls.tsx
+++ src/components/DevControls.tsx
@@ -5,60 +5,45 @@ import classes from '../css/App.module.css';
 import { FLEX_PROPERTIES } from 'constants/flexProperties';
 
 export const DevControls = () => {
-    const { dispatch } = useBlockContext();
-    const nodeRef = useRef<null | HTMLFormElement>(null);
+  const { dispatch, state } = useBlockContext();
+  const nodeRef = useRef<null | HTMLFormElement>(null);
 
-    const handleChange: FormEventHandler<HTMLFormElement> = ({ target }) => {
-        if (target instanceof HTMLInputElement) {
-            const { name, value } = target;
-            dispatch({ action: Actions.STYLE, value: { [name]: value } });
-        }
-    };
+  const handleChange: FormEventHandler<HTMLFormElement> = ({ target }) => {
+    if (target instanceof HTMLInputElement) {
+      const { name, value } = target;
+      dispatch({ action: Actions.STYLE, value: { [name]: value } });
+    }
+  };
 
-    return (
-        <Draggable nodeRef={nodeRef}>
-            <form
-                ref={nodeRef}
-                className={classes.devControls}
-                onChange={handleChange}
-            >
-                {Object.entries(FLEX_PROPERTIES.parent).map(
-                    ([property, values]) => (
-                        <div className={classes.flexGroup} key={property}>
-                            <p>{property}</p>
-                            <div className={classes.radioButtons}>
-                                {Object.keys(values).map((value, i) => (
-                                    <label key={`${property}${value}`}>
-                                        <input
-                                            type="radio"
-                                            value={value}
-                                            name={property}
-                                            defaultChecked={i === 0}
-                                        />
-                                        {value}
-                                    </label>
-                                ))}
-                            </div>
-                        </div>
-                    )
-                )}
-                {/* {Object.entries(FlexProperties).map(([property, values]) => (
-                    <Fragment key={property}>
-                        <p>{property}</p>
-                        {Object.values(values).map((value, i) => (
-                            <label key={`${property}${value}`}>
-                                <input
-                                    type="radio"
-                                    value={value}
-                                    name={property}
-                                    defaultChecked={i === 0}
-                                />
-                                {value}
-                            </label>
-                        ))}
-                    </Fragment>
-                ))} */}
-            </form>
-        </Draggable>
-    );
+  return (
+    <Draggable nodeRef={nodeRef}>
+      <form
+        ref={nodeRef}
+        className={classes.devControls}
+        onChange={handleChange}
+      >
+        {Object.entries(FLEX_PROPERTIES.parent).map(([property, values]) => (
+          <div className={classes.flexGroup} key={property}>
+            <p>{property}</p>
+            <div className={classes.radioButtons}>
+              {Object.keys(values).map((value) => (
+                <label key={`${property}${value}`}>
+                  <input
+                    type="radio"
+                    value={value}
+                    name={property}
+                    defaultChecked={
+                      state.parent[property as keyof typeof state.parent] ===
+                      value
+                    }
+                  />
+                  {value}
+                </label>
+              ))}
+            </div>
+          </div>
+        ))}
+      </form>
+    </Draggable>
+  );
 };

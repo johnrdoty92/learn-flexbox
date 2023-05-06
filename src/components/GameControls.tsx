503a17d87835e0401cd6083663b8dd376bb36687
John Doty <johnrdoty92@gmail.com>
2023-05-06 18:44:41 +0900

    add functionality to game play buttons

--- src/components/GameControls.tsx
+++ src/components/GameControls.tsx
@@ -2,8 +2,10 @@ import { FLEX_PROPERTIES } from 'constants/flexProperties';
 import { useState } from 'react';
 import classes from '../css/App.module.css';
 import { animated, config, useTransition } from '@react-spring/web';
+import { Actions, useBlockContext } from 'contexts/BlockContext';
 
 export const GameControls = () => {
+  const { dispatch } = useBlockContext();
   const [selectedFlexProp, setSelectedFlexProp] =
     useState<keyof (typeof FLEX_PROPERTIES)['parent']>('flexDirection');
   const flexProps = Object.keys(FLEX_PROPERTIES.parent[selectedFlexProp]).map(
@@ -25,7 +27,15 @@ export const GameControls = () => {
     <div className={classes.gameControlsContainer}>
       <div className={classes.flexPropertyValueBtnContainer}>
         {transitions((style, item) => (
-          <animated.button style={style}>
+          <animated.button
+            onClick={() =>
+              dispatch({
+                action: Actions.STYLE,
+                value: { [selectedFlexProp]: item.value },
+              })
+            }
+            style={style}
+          >
             <p className={classes.flexPropertyValueText}>{item.value}</p>
           </animated.button>
         ))}
